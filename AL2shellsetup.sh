5c90e2ecf4ad9d81093acbbd5cd8f1e5f376e74b
emerdenny <emerdeny@protonmail.ch>
2020-04-01 10:44:08 -0500

    AL2 shell setup update.

--- AL2shellsetup.sh
+++ AL2shellsetup.sh
@@ -3,35 +3,35 @@
 # This script sets up vagrant box bento/amazonlinux-2 for python development.
 
 # Make temporary ~/setup folder for easy cleanup
-mkdir ~/setup
-cd ~/setup
+mkdir /home/vagrant/setup
+cd /home/vagrant/setup
 
 # Configure git 
 git config --global user.name "emerdenny"
 git config --global user.email "emerdenny@protonmail.ch"
 
 # Transfer .zshrc
-cp ~/EWDscripts/config_files/.zshrc ~/.zshrc
+cp /home/vagrant/EWDscripts/config_files/.zshrc /home/vagrant/.zshrc
 
 # Install ZSH
 yum -q -y install zsh
 
 # Install Oh-My-ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended" 
 exit
 
 # Install Powerlevel9k Theme
-git clone -q https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone -q https://github.com/bhilburn/powerlevel9k.git /home/vagrant/.oh-my-zsh/custom/themes/powerlevel9k
 
 # Install Powerline Fonts
 git clone -q https://github.com/powerline/fonts.git
 ./fonts/install.sh
 
 # Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
 
 # Transfer .vimrc
-cp ~/EWDscripts/config_files/.vimrc ~/.vimrc
+cp /home/vagrant/EWDscripts/config_files/.vimrc /home/vagrant/.vimrc
 
 # Within Vim run :PluginInstall
 vim -c ":PluginInstall" -c ":q"
@@ -40,10 +40,10 @@ vim -c ":PluginInstall" -c ":q"
 yum -q -y install tmux
 
 # Transfer .tmux.conf
-cp ~/EWDscripts/config_files/.vimrc ~/.vimrc
+cp /home/vagrant/EWDscripts/config_files/.vimrc /home/vagrant/.vimrc
 
 # Remove setup directory
-rm -rf ~/setup
+rm -rf /home/vagrant/setup
 
 # Set ZSH as default shell
 vim passwordtest -c ":%s/"${USER}":\/bin\/bash/"${USER}":\/bin\/zsh/g" -c ":wq"
